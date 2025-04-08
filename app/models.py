0f75e32e109a2b212563fcdaace6bc22d219d88b
WendersonBarros <wendercesar2@gmail.com>
2025-04-07 23:04:46 -0400

    feat: creates initial database schema and migration

--- app/models.py
+++ app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Membership(models.Model):
+    name = models.CharField(max_length=200)
+    duration_in_months = models.IntegerField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    is_active = models.BooleanField()
+
+
+class Member(models.Model):
+    name = models.CharField(max_length=255)
+    birth_date = models.DateField()
+    entry_code = models.IntegerField(unique=True)
+    membership = models.ForeignKey(Membership, on_delete=models.RESTRICT)
+
+
+class Membership_status(models.Model):
+    user = models.ForeignKey(Member, on_delete=models.CASCADE)
+    current_membership = models.ForeignKey(Membership, on_delete=models.RESTRICT)
+    expiration_date = models.DateField()
